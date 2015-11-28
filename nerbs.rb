9f929b4dd2b0cceaadfa7dbf9a231edb974595ba
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-11-28 14:32:03 -0800

    working on slow network. might be hyper-v's fault

--- nerbs.rb
+++ nerbs.rb
@@ -1,19 +1,14 @@
 #!/usr/bin/env ruby
-puts "Gonna push w rsync"
-puts "Gonna compile with ssh remote command"
-puts "Gonna pull firmware with rsync"
-puts "Gonna run fwup for you"
-
 @here = Dir.pwd
 @name = File.basename(@here)
 
+@remote_cache_dir = "~/nerbsrv/cache"
 @remote_sdk_dir = "~/nerves-sdk"
 @sdk = "#{@remote_sdk_dir}/nerves-env.sh"
 @ssh_user = "keyvan"
 @ssh_host = "192.168.1.139"
 @ssh_target = "#{@ssh_user}@#{@ssh_host}"
-
-@remote_cache_dir = "~/nerbsrv/cache"
+@there = "#{@remote_cache_dir}/#{@name}"
 
 def init
   remote_system "mkdir -p #{@remote_cache_dir}"
@@ -22,7 +17,6 @@ end
 def push here
   there = "#{@ssh_target}:#{@remote_cache_dir}"
   system "rsync --progress -arv --exclude=.git #{here} #{there}"
-  "#{@remote_cache_dir}/#{@name}"
 end
 
 def remote_system cmd
@@ -33,12 +27,12 @@ def compile remote_dir
   remote_system "source #{@sdk} && cd #{remote_dir} && make"
 end
 
-def pull remote_dir, here
-  there = "#{@ssh_target}:#{remote_dir}/_images"
+def pull sub_dir, here
+  there = "#{@ssh_target}:#{@there}/#{sub_dir}"
   system "rsync --progress -arv #{there} #{here}"
 end
 
-init
-@there = push @here
-compile @there
-pull @there, @here
+#init
+#push @here
+#compile @there
+pull :_images, @here
